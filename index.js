76ac64a291a6144fce6d8a2794454a2989ac7b27
zacharskim <matthew.zacharski@gmail.com>
2025-04-17 21:56:47 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,36 @@
+// filepath: /Users/mattzacharski/Desktop/my-electron-app/index.js
+const { app, BrowserWindow } = require("electron");
+
+let mainWindow;
+
+app.on("ready", () => {
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  });
+
+  mainWindow.loadFile("index.html"); // Ensure you create an index.html file as well
+});
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
+
+app.on("activate", () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    mainWindow = new BrowserWindow({
+      width: 800,
+      height: 600,
+      webPreferences: {
+        nodeIntegration: true
+      }
+    });
+
+    mainWindow.loadFile("index.html");
+  }
+});
---
+++ main.js
@@ -0,0 +1,70 @@
+// filepath: /Users/mattzacharski/Desktop/my-electron-app/main.js
+const { app, Tray, Menu, nativeImage, BrowserWindow } = require("electron");
+const path = require("path");
+
+// Enable HMR
+require("electron-reload")(__dirname, {
+  electron: require(`${__dirname}/node_modules/electron`)
+});
+
+let tray = null;
+let window = null;
+
+app.whenReady().then(() => {
+  const icon = nativeImage.createFromPath(path.join(__dirname, "image copy.png"));
+  tray = new Tray(icon);
+  const contextMenu = Menu.buildFromTemplate([
+    { label: "Item1", type: "radio" },
+    { label: "Item2", type: "radio" },
+    { label: "Item3", type: "radio", checked: true },
+    { label: "Item4", type: "radio" }
+  ]);
+
+  tray.setContextMenu(contextMenu);
+  tray.setToolTip("This is my application");
+
+  createWindow();
+});
+
+function createWindow() {
+  window = new BrowserWindow({
+    width: 300,
+    height: 400,
+    show: false,
+    frame: false,
+    resizable: false,
+    fullscreenable: false,
+    transparent: true,
+    alwaysOnTop: true,
+    skipTaskbar: true,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false
+    }
+  });
+
+  window.loadFile("index.html");
+}
+
+function toggleWindow() {
+  if (window.isVisible()) {
+    window.hide();
+  } else {
+    const { x, y } = tray.getBounds();
+    const { width, height } = window.getBounds();
+
+    window.setBounds({
+      x: x - Math.floor(width / 2),
+      y: y + 25,
+      width,
+      height
+    });
+
+    window.show();
+    window.focus();
+  }
+}
+
+app.on("window-all-closed", (e) => {
+  e.preventDefault(); // don't quit when window is closed
+});
